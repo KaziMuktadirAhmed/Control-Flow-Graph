c2795307687d6a6795e2ef451df3ddbcc26d0b73
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-16 12:50:25 +0600

    add parser classes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,17 @@
 package Main;
 
+import Parser.Parser;
+
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello world!");
+        test();
+    }
+
+    public static void test() throws FileNotFoundException {
+        Parser parse_file = new Parser("test.txt");
+        parse_file.printFile();
     }
 }
--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -2,20 +2,32 @@ package Parser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
-    private String original_input;
+    private final String original_input;
+    private final ArrayList<String> lines = new ArrayList<>();
 
     public Parser(String filePath) throws FileNotFoundException {
         this.original_input = fileToString(filePath);
     }
 
-    public String fileToString(String filePath) throws FileNotFoundException {
+    private String fileToString(String filePath) throws FileNotFoundException {
         String fileString = "";
         File input_file = new File(filePath);
         Scanner scan_file = new Scanner(input_file);
-        while (scan_file.hasNextLine()) fileString += scan_file.nextLine();
+        while (scan_file.hasNextLine()){
+            String temp_line = scan_file.nextLine();
+            fileString += temp_line;
+            lines.add(temp_line);
+        }
         return fileString;
+    }
+
+    public void printFile () {
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
