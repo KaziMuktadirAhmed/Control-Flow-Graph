cd4d678cd766953399019f01f0a3880101f82238
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-16 23:20:18 +0600

    add CFG node

---
+++ src/CFG/Node.java
@@ -0,0 +1,18 @@
+package CFG;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
+public class Node {
+    private String name;
+    private String line;
+    public ArrayList<Node> childs;
+
+    public Node(String line) {
+        this.line = line;
+    }
+
+    public void setName(String name) { this.name = name; }
+    public String name() {  return this.name;   }
+    public String line() {  return this.line;   }
+}
