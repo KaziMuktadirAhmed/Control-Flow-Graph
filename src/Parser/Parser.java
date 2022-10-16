4d763d2879db4ebcecb2ad5a7b916dc0a179a165
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-16 12:26:58 +0600

    add parser classes

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,9 +1,21 @@
 package Parser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Parser {
     private String original_input;
 
-    public Parser(String input) {
-        this.original_input = input;
+    public Parser(String filePath) throws FileNotFoundException {
+        this.original_input = fileToString(filePath);
+    }
+
+    public String fileToString(String filePath) throws FileNotFoundException {
+        String fileString = "";
+        File input_file = new File(filePath);
+        Scanner scan_file = new Scanner(input_file);
+        while (scan_file.hasNextLine()) fileString += scan_file.nextLine();
+        return fileString;
     }
 }
