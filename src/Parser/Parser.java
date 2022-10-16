4493367caf327468341b682d488757309daf8801
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-16 12:52:48 +0600

    add parser classes

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -6,23 +6,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
-    private final String original_input;
     private final ArrayList<String> lines = new ArrayList<>();
 
     public Parser(String filePath) throws FileNotFoundException {
-        this.original_input = fileToString(filePath);
+        fileToString(filePath);
     }
 
-    private String fileToString(String filePath) throws FileNotFoundException {
-        String fileString = "";
+    private void fileToString(String filePath) throws FileNotFoundException {
         File input_file = new File(filePath);
         Scanner scan_file = new Scanner(input_file);
-        while (scan_file.hasNextLine()){
-            String temp_line = scan_file.nextLine();
-            fileString += temp_line;
-            lines.add(temp_line);
-        }
-        return fileString;
+        while (scan_file.hasNextLine()) lines.add(scan_file.nextLine());
     }
 
     public void printFile () {
