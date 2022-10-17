8e52bfc8555ae4f0c44a419272296b86310f6bf5
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-17 23:16:57 +0600

    add Do while check and back edge

--- src/CFG/Node/WHILEBlock.java
+++ src/CFG/Node/WHILEBlock.java
@@ -2,9 +2,24 @@ package CFG.Node;
 
 public class WHILEBlock extends Node{
     public String condition;
+    private boolean isDoWhile = false;
+    private Node pointToDo = null;
 
     public WHILEBlock(String line, String condition) {
         super(line);
         this.condition = condition;
+    }
+
+    public boolean isDoWhile() {
+        return this.isDoWhile;
+    }
+
+    public void setPointToDo(Node DoBlock) {
+        this.isDoWhile = true;
+        this.pointToDo = DoBlock;
+    }
+
+    public Node gotoDoBlock() {
+        return this.pointToDo;
     }
 }
